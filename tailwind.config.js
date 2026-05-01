6daa90341a9e1ef17d749d64bd372c37014d9a99
Demo-proyects <lunarojadigital@gmail.com>
2026-05-01 14:27:19 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./*.html", "./js/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#bf7c1a',
+        'gold-lt': '#d4952a',
+        'gold-dk': '#9a6010',
+        twht: '#f0ece4',
+        tgry: '#8a8580',
+      },
+      fontFamily: {
+        rye: ['Rye', 'serif'],
+        rally: ['Raleway', 'sans-serif'],
+      }
+    }
+  },
+  plugins: [],
+}
